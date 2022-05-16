f336286822caf3690133287e6fbf0adf765f3e01
Yana Glad <55493845+YanaGlad@users.noreply.github.com>
2022-05-16 11:15:49 +0300

    Sign up button

--- app/src/main/java/com/example/nomoneytrade/screen/AuthorizeScreen.kt
+++ app/src/main/java/com/example/nomoneytrade/screen/AuthorizeScreen.kt
@@ -4,11 +4,15 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ExtendedFloatingActionButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
@@ -18,11 +22,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -65,5 +71,28 @@ fun AuthorizeScreen() {
 
         UiUtilsTextField(label = "email", padding = 15)
         UiUtilsTextField(label = "password", padding = 15)
+
+        ExtendedFloatingActionButton(
+            onClick = {
+                //TODO navigation to sign up
+            },
+            modifier = Modifier
+                .padding(10.dp)
+                .padding(top = 15.dp)
+                .wrapContentWidth()
+                .align(CenterHorizontally),
+
+            backgroundColor = MaterialTheme.colors.primary,
+            text = {
+                Text(
+                    text = "Sign up",
+                    fontSize = 15.sp,
+                    color = MaterialTheme.colors.onPrimary,
+                    modifier = Modifier
+                        .weight(5f)
+                        .wrapContentWidth(),
+                )
+            }
+        )
     }
 }

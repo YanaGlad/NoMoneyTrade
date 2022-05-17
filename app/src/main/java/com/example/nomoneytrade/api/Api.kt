39fe5c49955cdf4c81e22a2d7aae865ac067fdd6
Yana Glad <55493845+YanaGlad@users.noreply.github.com>
2022-05-17 07:38:40 +0300

    Hilt setup

---
+++ app/src/main/java/com/example/nomoneytrade/api/Api.kt
@@ -0,0 +1,8 @@
+package com.example.nomoneytrade.api
+
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface Api {
+
+}
---
+++ app/src/main/java/com/example/nomoneytrade/di/RetrofitModule.kt
@@ -0,0 +1,45 @@
+package com.example.nomoneytrade.di
+
+import com.example.nomoneytrade.api.Api
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import kotlinx.serialization.json.Json
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+const val BASE_URL = ""
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+
+    @Provides
+    @Singleton
+    fun getRetrofit(): Retrofit {
+        val okHttpBuilder: OkHttpClient.Builder = OkHttpClient.Builder()
+            .connectTimeout(5, TimeUnit.MINUTES)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
+
+        val contentType = MediaType.get("application/json")
+        val json = Json { ignoreUnknownKeys = true }
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(json.asConverterFactory(contentType))
+            .client(okHttpBuilder.build())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun getApi(retrofit: Retrofit): Api {
+        return retrofit.create(Api::class.java)
+    }
+}
